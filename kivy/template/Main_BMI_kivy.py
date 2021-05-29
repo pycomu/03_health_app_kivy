1de2883b84081228dea6aa92f02148c1ae49b87a
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2021-05-29 13:49:28 +0200

    Merge branch 'main' of https://github.com/pycomu/03_health_app_kivy

--- kivy/template/Main_BMI_kivy.py
+++ kivy/template/Main_BMI_kivy.py
@@ -1,13 +1,19 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
 
+from home4screen import Home4Screen # import the functions used in this screen 
+
 from kivy.core.window import Window
 Window.size = (400, 750)
 
+class Home4Screen(Screen):
+    pass
+
+class Home3Screen(Screen):
+    pass
 
 class Home2Screen(Screen):
     pass
-
 
 class HomeScreen(Screen):
     pass
--- kivy/template/home4screen.py
+++ kivy/template/home4screen.py
@@ -1,28 +1,14 @@
 from kivymd.app import MDApp
-from kivymd.uix.floatlayout import MDFloatLayout
+from kivy.uix.screenmanager import Screen
 
-from kivy.core.window import Window
-Window.size = (400, 750)
+from kivy.network.urlrequest import UrlRequest  # specific for wiki API
+import certifi                                  # specific for wiki API
 
-from kivy.network.urlrequest import UrlRequest
-import certifi
+class Home4Screen(Screen):              # this class is initiated in main.py file already
 
-class TestPage(MDFloatLayout): # Screen "TestPage"
-    pass
-    
-    
-class testtemplateApp(MDApp): # design in testtemplate.kv the screen
-
-    def on_start(self):
-        #https://kivymd.readthedocs.io/en/latest/themes/theming/
-        self.theme_cls.primary_palette = 'Blue'
-        self.theme_cls.accent_palette = 'Blue'
-        self.theme_cls.primary_hue = "500"
-        self.theme_cls.theme_style = "Light"
-    
-    def wiki_search_button(self):
-        query = self.root.ids["search_wiki"].text
-        if self.root.ids.search_wiki.text != "":
+    def wiki_search(self):
+        query = self.ids["search_wiki"].text
+        if self.ids.search_wiki.text != "":
             self.get_data(title=query)
 
     def get_data(self, *args, title=None):
@@ -43,12 +29,5 @@ class testtemplateApp(MDApp): # design in testtemplate.kv the screen
             content = page_info[page_id]["extract"]
         except KeyError:
             content = f"Sorry, but your search '{page_title}' got no results!\n\nPlease try again! "
-        self.root.ids["search_result"].text = f"{page_title}\n\n{content}"
+        self.ids["search_result"].text = f"{page_title}\n\n{content}"
         
-
-    def build(self):
-        return TestPage() # read in the kv-file and build the screen
-
-if __name__ == '__main__':
-    app = testtemplateApp()
-    app.run()

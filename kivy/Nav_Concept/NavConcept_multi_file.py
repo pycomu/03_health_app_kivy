d003cd936df109417668ce2c30de1fc47c7fffa0
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2021-06-14 00:13:45 +0200

    Navi Concept

---
+++ kivy/Nav_Concept/NavConcept_multi_file.py
@@ -0,0 +1,32 @@
+from kivymd.app import MDApp
+# from kivy.lang import Builder # no need at multi files
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.properties import ObjectProperty
+from kivymd.uix.picker import MDThemePicker
+
+from kivymd.uix.list import OneLineIconListItem, MDList
+
+from kivy.core.window import Window
+Window.size = (400, 750)
+
+
+
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ObjectProperty() # because in seperate kv file
+    nav_drawer = ObjectProperty()
+    
+    def show_theme_picker(self):
+        theme_dialog = MDThemePicker()
+        theme_dialog.open()
+
+class Def_Screen(BoxLayout): # name of screen in navconcept_multi_file.kv
+    pass
+
+class NavConcept_multi_file(MDApp):
+    
+    def build(self):
+        return Def_Screen()
+
+
+NavConcept_multi_file().run()
---
+++ kivy/Nav_Concept/NavConcept_one_file.py
@@ -0,0 +1,141 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.properties import ObjectProperty
+from kivymd.uix.picker import MDThemePicker
+
+from kivymd.uix.list import OneLineIconListItem, MDList
+
+from kivy.core.window import Window
+Window.size = (400, 750)
+
+KV = '''
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/kivy-icon-256.png"
+
+    MDLabel:
+        text: "Children Health App"
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: "by PyCoMu"
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    ScrollView:
+        
+        MDList:
+            id: md_list
+
+            OneLineListItem:
+                text: "Screen 1"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "Screen1"
+
+            OneLineListItem:
+                text: "Screen 2"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "Screen2"
+
+            OneLineIconListItem:
+                text: "Theme"
+                on_press:
+                    root.show_theme_picker()
+                IconLeftWidget:
+                    icon: 'theme-light-dark'
+
+Screen:
+
+    MDNavigationLayout:
+
+        ScreenManager:
+            id: screen_manager
+
+            Screen:
+                name: "Screen1"
+                id: screen1
+                
+                BoxLayout:
+                    orientation: 'vertical'
+
+                    # try to put as template ond day ?
+                    MDToolbar: 
+                        title: "Toolbar of Screen 1"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+
+                    BoxLayout: # Boxlayout for all other widgets on screen 1
+                        orientation: 'vertical'
+                        MDLabel:
+                            text: "Screen 1 with toolbar"
+                            halign: "center" 
+                        MDRaisedButton:
+                            text: "back to Screen 2 without toolbar"
+                            pos_hint:{'center_x': 0.5, 'center_y': 0.4}
+                            on_release:
+                                screen_manager.current = "Screen2"
+            Screen:
+                name: "Screen2"
+                id: screen2
+                
+                BoxLayout:
+                    orientation: 'vertical'
+
+                    BoxLayout: # Boxlayout for all other widgets on screen 1
+                        orientation: 'vertical'
+                        MDLabel:
+                            text: "Screen 2 without toolbar"
+                            halign: "center"
+                        MDRaisedButton:
+                            text: "back to Screen 1 with toolbar"
+                            pos_hint:{'center_x': 0.5, 'center_y': 0.4}
+                            on_release:
+                                screen_manager.current = "Screen1"
+
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
+
+      
+'''
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
+    # screen_manager = ObjectProperty() # because in seperate kv file
+    # nav_drawer = ObjectProperty()
+    
+    def show_theme_picker(self):
+        theme_dialog = MDThemePicker()
+        theme_dialog.open()
+
+
+
+class NavConcept_one_file(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+
+NavConcept_one_file().run()
